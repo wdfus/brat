d9825faa06aee19c698d0d20a67220d65016baad
vanechek777 <glmdv.ivan@gmail.com>
2025-09-20 21:19:06 +0900

    vova gay

--- Brat/MainWindow.xaml.cs
+++ Brat/MainWindow.xaml.cs
@@ -20,5 +20,15 @@ namespace Brat
         {
             InitializeComponent();
         }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void ListBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+
+        }
     }
 }
